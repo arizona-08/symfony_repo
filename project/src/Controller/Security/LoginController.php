0670893372420883f0be2b02601a16c1acdeb435
Jonathan Assi <assijonathan2@gmail.com>
2024-11-28 20:11:07 +0100

    Ajout du login et logout. Intégrer la logique du login dans le template du login du streemi

--- project/src/Controller/Security/LoginController.php
+++ project/src/Controller/Security/LoginController.php
@@ -6,14 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route("/login")]
+// #[Route("/login")]
 class LoginController extends AbstractController{
-    #[Route(path: '/', name: "login")]
-    public function login(): Response
-    {
-        //retourne la vue pour le login
-        return $this->render("auth/login.html.twig");
-    }
+    // #[Route(path: '/', name: "login")]
+    // public function login(): Response
+    // {
+    //     //retourne la vue pour le login
+    //     return $this->render("auth/login.html.twig");
+    // }
 
     #[Route(path: '/confirm', name: "confirm")]
     public function confirm(): Response
